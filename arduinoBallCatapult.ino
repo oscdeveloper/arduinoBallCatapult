54606f94a25ffb5ee66c5476eac0a2667bce2f5f
Arkadiusz Krakiewicz <biuro@oscdeveloper.pl>
2017-08-06 01:06:10 +0200

    led + servo

--- arduinoBallCatapult.ino
+++ arduinoBallCatapult.ino
@@ -10,19 +10,28 @@
 
 #define PIN_POTENTIOMETER A0
 
-//String readString;
-//#include <Servo.h>
-//Servo rotatingServo;  // create servo object to control a servo
+#define PIN_ARM_CHECK 8
+
+#define PIN_DOG_BUTTON 7
+
+#define PIN_POWER_SERVO 3
+
+#include <Servo.h>
+Servo rotatingServo;  // create servo object to control a servo
 
 int rotationTime = 2; // min rotating time
 int potentiometerValue = 0;
 int oldPotentiometerValue = 0;
 
 void setup() {
-  Serial.begin(9600);
+  Serial.begin(9600);  
+  rotatingServo.attach(PIN_POWER_SERVO);  //the pin for the servo control
   //rotatingServo.write(90); //set initial servo position
-  //rotatingServo.attach(3);  //the pin for the servo control
   Serial.println("catapult started"); // so I can keep track of what is loaded
+
+  pinMode(PIN_ARM_CHECK, INPUT_PULLUP);
+
+  pinMode(PIN_DOG_BUTTON, INPUT_PULLUP);
 
   // start short two beeps after power on
   pinMode(PIN_BUZZER, OUTPUT);
@@ -40,7 +49,6 @@ void setup() {
   pinMode(PIN_LED_STATUS_G, OUTPUT);
   if (PIN_LED_STATUS_B != false) pinMode(PIN_LED_STATUS_B, OUTPUT);
   setColor('s', 0, 10, 0); 
-  //digitalWrite(10, HIGH);
   // end status led after power on
 
   // start distance led after power on
@@ -57,6 +65,46 @@ void loop() {
 
   potentiometerValue = analogRead(PIN_POTENTIOMETER);
 
+  if ( potentiometerValue != oldPotentiometerValue ) {
+  
+    oldPotentiometerValue = potentiometerValue;
+
+    rotationTime = getRotationTime(potentiometerValue);
+  
+    setDistanceLedColor(rotationTime); 
+  
+    //rotatingServo.write(81);
+    //delay(rotationTime * 1000);
+    //rotatingServo.write(90); // stop servo rotating
+    //Serial.println(potentiometerValue);
+    
+  }
+
+  
+
+if ( digitalRead(PIN_ARM_CHECK) == LOW ) { // arm is ready to shoot
+  setColor('s', 0, 10, 0); // green led
+
+  if ( digitalRead(PIN_DOG_BUTTON) == LOW ) { // dog button pushed, arm is loading 
+Serial.println(rotationTime);
+    setColor('s', 10, 0, 0); // red led
+    //delay(500);
+    //setColor('s', 0, 0, 0);
+    //delay(500);
+    //rotatingServo.write(81);
+    delay(rotationTime * 1000);
+    //rotatingServo.write(90); // stop servo rotating
+    Serial.println("servo trigger");
+    
+  }
+  
+} else { // arm is lowering
+  setColor('s', 30, 1, 0); // orange led
+  delay(500);
+  setColor('s', 0, 0, 0);
+  delay(500);
+}
+
 // 104 servo max left
 // 90 stop servo
 // 81 sero max right
@@ -64,18 +112,7 @@ Serial.println(potentiometerValue);
 
 //myservo.write(n);
 
-if ( potentiometerValue != oldPotentiometerValue ) {
 
-  oldPotentiometerValue = potentiometerValue;
-
-  setDistanceLedColor(getRotationTime(potentiometerValue)); 
-
-  //rotatingServo.write(81);
-  //delay(rotationTime * 1000);
-  //rotatingServo.write(90); // stop servo rotating
-  //Serial.println(potentiometerValue);
-  
-}
 
 
   
@@ -97,16 +134,16 @@ void setColor(char ledType, int red, int green, int blue) {
 void setDistanceLedColor(int rotationTime) {
   switch (rotationTime) {
     case 2:
-      setColor('d', 0, 1, 0); 
+      setColor('d', 0, 1, 0); // green
       break;
     case 3:
-      setColor('d', 5, 1, 0); 
+      setColor('d', 5, 1, 0); // light orange
       break;
     case 4:
-      setColor('d', 30, 1, 0); 
+      setColor('d', 30, 1, 0); // strong orange
       break;
     case 5:
-      setColor('d', 50, 0, 0); 
+      setColor('d', 50, 0, 0); // red
       break;
   }  
 }
